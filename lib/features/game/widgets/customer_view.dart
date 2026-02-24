0a9093ae178f37709ba3910f58d2bbe71681cf68
Stuart <sgardoll@gmail.com>
2026-02-24 17:18:17 +1100

    feat(03-01): build CustomerView widget

---
+++ lib/features/game/widgets/customer_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:mindsort/core/models/customer.dart';
+
+class CustomerView extends StatelessWidget {
+  final Customer customer;
+
+  const CustomerView({super.key, required this.customer});
+
+  @override
+  Widget build(BuildContext context) {
+    final patienceRatio = customer.currentPatience / customer.maxPatience;
+    final color = patienceRatio > 0.5 
+        ? Colors.green 
+        : (patienceRatio > 0.25 ? Colors.orange : Colors.red);
+
+    return Container(
+      width: 60,
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      decoration: BoxDecoration(
+        color: const Color(0xFF37474F),
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: color, width: 2),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              customer.order.emoji,
+              style: const TextStyle(fontSize: 24),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(6),
+              bottomRight: Radius.circular(6),
+            ),
+            child: LinearProgressIndicator(
+              value: patienceRatio,
+              color: color,
+              backgroundColor: Colors.black26,
+              minHeight: 6,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
